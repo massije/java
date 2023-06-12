a9a0e8ea43a80853323d0e8bb19bab6be89b2fcb
massije <jedc.marti@gmail.com>
2023-06-12 18:07:33 +0200

    02-Section::Variables

---
+++ udemy-java/02-Section/MyVariables/Variables.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+
+class Variables {
+   
+   public static void main(String args[]){
+      
+      byte a = 120;
+      
+      System.out.println(a);
+      
+      int numero = 23;
+      
+      System.out.println("This is a data of type int: " + numero);
+  
+   }
+}
