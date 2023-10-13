29a2a499fa6dfa4f6fa7130787f4dae6688e1da4
massije <jedc.marti@gmail.com>
2023-10-13 13:30:32 +0200

    00-Section::More-tests

--- udemy-java/00-Tests/Tests.java
+++ udemy-java/00-Tests/Tests.java
@@ -1,13 +1,19 @@
 import java.lang.*;
+import java.util.Scanner;
 
 class Tests {
-
-   public static void main(String args[]){
-   
-   
-      System.out.println("hello world");
-      
-   
+   public static void main(String[] args) {
+      for (int line = 1; line <= 5; line++) {
+         for (int raya = 0; raya <= 5 - line; raya++) {
+            System.out.print("-");
+         }
+         for (int i = 1; i <= (2 * line - 1); i++) {
+            System.out.print(2 * line - 1);
+         }
+         for (int raya = 0; raya <= 5 - line; raya++) {
+            System.out.print("-");
+         }
+         System.out.println();
+      }
    }
-
 }
