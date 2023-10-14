565b8d6a10e0b1f9fa362f93abe2dea1d0e44a55
massije <jedc.marti@gmail.com>
2023-10-14 06:12:57 +0200

    00-Section::piramide-terminado-con-redundancias

--- udemy-java/00-Tests/Tests.java
+++ udemy-java/00-Tests/Tests.java
@@ -2,72 +2,87 @@ import java.lang.*;
 import java.util.Scanner;
 
 class Tests {
+
+   static final String TUBO = "||";
+   static final char ESPACIO = ' ';
+   static final String ladoIzquierdo = "_/";
+   static final String ladoDerecho = "\\_";
+   
    public static void main(String[] args) {
-   
-   for (int linea = 1; linea <= 4; linea++) { 
-      // figura __/
-      for (int space = 1; space <= (12 - 3 * linea); space++) {
-         System.out.print(" ");
-      }
-      System.out.print("__/");
-      
-      // figura de 2 puntos :
-      for (int colon = 1; colon <= (3 * linea - 3); colon++) {
-         System.out.print(":");
-      }
-      
-      // figura ||
-      for (int rayaVertical = 1; rayaVertical <= 2; rayaVertical++) {
-         System.out.print("|");
-      }
-      
-      // figura de 2 puntos :
-      for (int colon = 1; colon <= (3 * linea - 3); colon++) {
-         System.out.print(":");
-      }
-      
-      // figura \__
-      System.out.print("\\__");
-      
-      System.out.println();
-   }
-
-//    for (int linea = 1; linea <= 4; linea++) {
-//       
-//    }
-//    
-   
-          
-//          __/||\__
-//       __/:::||:::\__
-//    __/::::::||::::::\__
-// __/:::::::::||:::::::::\__
-// |""""""""""""""""""""""""|
-
-// 12 de distancia hasta esto ||
-
-
+      dibujarTorre();
    }
    
-   public static void packCuatroLineasVerticales() {
+   public static void dibujarTorre() {
+      dibujarCuatroLineasVerticales();
+      dibujarPiramide();
+      dibujarPiramideInvertida();
+      dibujarDoceLineasVerticales();
+      dibujarPiramide();
+   }
+   
+   public static void dibujarDoceEspacios() {
+      for (int espacio = 1; espacio <= 12; espacio++) {
+         System.out.print(ESPACIO);
+      }
+   }
+   
+   public static void dibujarCuatroLineasVerticales() {
       for (int linea = 1; linea <= 4; linea++){
-         for ( int espacio = 1; espacio <= 9; espacio++) {
-            System.out.print(" ");
+         dibujarDoceEspacios();
+         System.out.println(TUBO);
+      }
+   }
+   
+   public static void dibujarDoceLineasVerticales() {
+      for (int grupo = 1; grupo <= 3; grupo++) {
+         dibujarCuatroLineasVerticales();
+      }
+   }
+   
+   public static void dibujarPiramideInvertida() {
+      String uveInvertida = "/\\";
+      for (int linea = 1; linea <= 4; linea++) {
+         for (int space = 1; space <= (2 * linea - 2); space++) {
+            System.out.print(ESPACIO);
          }
-         System.out.println("||");
+         //Figura \_
+         System.out.print(ladoDerecho);
+         // Figura /\
+         for (int vInvertida = 1; vInvertida <= (11 - (2 * linea - 2)); vInvertida++) {
+            System.out.print(uveInvertida);
+         }
+         System.out.println(ladoIzquierdo);
       }
    }
    
-   public static void packDoceLineasVerticales() {
-      for (int pack = 1; pack <= 3; pack++) {
-         packCuatroLineasVerticales();
+   public static void dibujarPiramide() {
+      char rayaVertical = '|';
+      char dosPuntos = ':';
+      char barraBaja = '_';
+      char comillaDoble = '\"';
+      for (int linea = 1; linea <= 4; linea++) { 
+         // figura __/
+         for (int space = 1; space <= (12 - 3 * linea); space++) {
+            System.out.print(ESPACIO);
+         }
+         System.out.print(barraBaja + ladoIzquierdo);
+         // figura de 2 puntos :
+         for (int colon = 1; colon <= (3 * linea - 3); colon++) {
+            System.out.print(dosPuntos);
+         }
+         System.out.print(TUBO);
+         // figura de 2 puntos :
+         for (int colon = 1; colon <= (3 * linea - 3); colon++) {
+            System.out.print(dosPuntos);
+         }
+         // figura \__
+         System.out.println(ladoDerecho + barraBaja);
       }
+      // figura |""""""""""""""""""""""""|
+      System.out.print(rayaVertical);
+      for (int comillas = 1; comillas <= 24; comillas++) {
+         System.out.print(comillaDoble);
+      }
+      System.out.println(rayaVertical);
    }
-   
-   public static void piramide() {
-   
-   
-   }
-   
-   
 }
